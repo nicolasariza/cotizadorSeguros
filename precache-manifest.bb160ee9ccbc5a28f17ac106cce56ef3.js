6b706bc7727789a392ee26e8c34f507752506301
nicolasariza <nicolas_ariza@outlook.com>
2020-03-29 12:56:45 -0500

    Updates

---
+++ precache-manifest.bb160ee9ccbc5a28f17ac106cce56ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e7f3e70e6321017db0641d4992511a",
+    "url": "/cotizadorSeguros/index.html"
+  },
+  {
+    "revision": "0efc8c5c38a368d026a3",
+    "url": "/cotizadorSeguros/static/css/main.e1a4991a.chunk.css"
+  },
+  {
+    "revision": "793e9651e1296b14d801",
+    "url": "/cotizadorSeguros/static/js/2.67661ba4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cotizadorSeguros/static/js/2.67661ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0efc8c5c38a368d026a3",
+    "url": "/cotizadorSeguros/static/js/main.caea4d1a.chunk.js"
+  },
+  {
+    "revision": "7ca6792ef424ee76f957",
+    "url": "/cotizadorSeguros/static/js/runtime-main.9e83050c.js"
+  }
+]);
